4d7be8d5bb682caf180764deb10058814019277b
thienphuc <tthienphuc1612@gmail.com>
2025-09-14 18:53:08 +0700

    feat: add UpdateUserInfo servlet

--- src/main/java/controller/UpdateUserInfo.java
+++ src/main/java/controller/UpdateUserInfo.java
@@ -1,4 +1,72 @@
 package controller;
 
-public class updateUserInfo {
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import model.User;
+import service.UserService;
+import util.PasswordUtil;
+
+public class UpdateUserInfo extends HttpServlet {
+    UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        try {
+            updateInfoUser(request,response);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void updateInfoUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+        User sessionUser = (User) request.getSession().getAttribute("user");
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String newPassword = request.getParameter("newPassword");
+
+        User user = new User();
+        user.setId(sessionUser.getId());
+
+        if (name != null) {
+            user.setName(name);
+        }
+        else {
+            user.setName(sessionUser.getName());
+        }
+
+        if (email != null) {
+            user.setEmail(email);
+        }
+        else {
+            user.setEmail(sessionUser.getEmail());
+        }
+
+        if (phone != null) {
+            user.setPhone(phone);
+        }
+        else {
+            user.setPhone(sessionUser.getPhone());
+        }
+
+        if (newPassword != null) {
+            String newPasswordHash = PasswordUtil.hashPassword(newPassword);
+            user.setPasswordHash(newPasswordHash);
+        }
+
+        userService.updateUser(user);
+        response.sendRedirect("/user/info");
+    }
 }
